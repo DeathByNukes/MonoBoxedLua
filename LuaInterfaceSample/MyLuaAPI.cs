be053239e550cc4ba4d787f6f2a3401885061879
Elis?e Maurer <elisee@sparklin.org>
2013-02-25 11:12:33 +0100

    Add LuaInterfaceSample

---
+++ LuaInterfaceSample/MyLuaAPI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LuaInterface;
+
+namespace LuaInterfaceSample
+{
+    class MyLuaAPI
+    {
+        public Lua      Lua;
+
+        public MyLuaAPI()
+        {
+            Lua = new LuaInterface.Lua();
+            Type self = GetType();
+
+            // Expose some C# functions to Lua
+            Lua.NewTable( "MyLuaNameSpace" );
+            Lua.RegisterFunction( "MyLuaNameSpace.CoolFunction", this, self.GetMethod( "MyCoolFunction" ) );
+        }
+ 
+        public void MyCoolFunction( int count )
+        {
+            Console.WriteLine( "Got {0}!", count );
+        }
+    }
+}
---
+++ LuaInterfaceSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LuaInterfaceSample
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var luaSample = new MyLuaAPI();
+
+            luaSample.Lua.DoString( "MyLuaNameSpace.CoolFunction(5)" );
+        }
+    }
+}
