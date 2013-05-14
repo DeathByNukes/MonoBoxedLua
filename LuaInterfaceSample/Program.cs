d1823ca9fea456f07e80099cb65f4c65b2b6fd7f
Elis?e Maurer <elisee@sparklin.org>
2013-05-14 23:40:05 +0200

    Rebuild liblua-linux*.so with debug lib enabled

---
+++ LuaInterfaceSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LuaInterfaceSample
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var luaSample = new MyLuaAPI();
+
+            luaSample.Lua.DoString( "MyLuaNameSpace.CoolFunction(5)" );
+            luaSample.Lua.DoString( "local myVector = { x=2, y=5 }; print( MyLuaNameSpace.GetVectorLength( myVector ) )" );
+        }
+    }
+}
