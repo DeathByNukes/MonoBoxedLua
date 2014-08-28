200710335ea1001fc449c22825c084903b958260
DeathByNukes <DeathByNukes@gmail.com>
2014-08-27 20:26:04 -0500

    Improved naming in Table unit test and added case sensitivity check.

--- LuaInterfaceTest/Table.cs
+++ LuaInterfaceTest/Table.cs
@@ -25,7 +25,8 @@ namespace LuaInterfaceTest
 							three = 3,
 							four = 4,
 							five = 5,
-							six = 6,
+							SIX = 6,
+							six = 'wrong',
 							[1/2] = 'half', -- double key
 						}, meta  )
 					end
@@ -39,15 +40,15 @@ namespace LuaInterfaceTest
 			return lua;
 		}
 
-		const int TestArrayEnd = 6;
-		readonly IList<string> TestKeys = Array.AsReadOnly(new [] { "one", "two", "three", "four", "five", "six" });
-		const double TestDouble = 1.0/2;
-		const string TestDoubleValue = "half";
+		const int _ArrayEnd = 6;
+		readonly IList<string> _Keys = Array.AsReadOnly(new [] { "one", "two", "three", "four", "five", "SIX" });
+		const double _Double = 1.0/2;
+		const string _DoubleValue = "half";
 
-		readonly IList<string> TestEmptyKeys = Array.AsReadOnly(new [] { "foo", "bar", "baz", "qux" });
-		const double TestEmptyDouble = 1.0 / 4;
-		const string TestMetaRead = "metatable read";
-		const string TestMetaWrite = "metatable write";
+		readonly IList<string> _EmptyKeys = Array.AsReadOnly(new [] { "foo", "bar", "baz", "qux", "ONE", "One" });
+		const double _EmptyDouble = 1.0 / 4;
+		const string _MetaRead = "metatable read";
+		const string _MetaWrite = "metatable write";
 
 
 		[TestMethod] public void Get()
@@ -57,26 +58,26 @@ namespace LuaInterfaceTest
 			{
 				// - try reading normal values, which the metatable can't intercept -
 
-				Assert.AreEqual(TestArrayEnd, table.Length);
+				Assert.AreEqual(_ArrayEnd, table.Length);
 
 				// array
-				for (double i = 1; i <= TestArrayEnd; ++i)
+				for (double i = 1; i <= _ArrayEnd; ++i)
 					Assert.AreEqual(i, (double)table[i]);
 
 				// associative array
-				for (int i = 0; i < TestKeys.Count; ++i)
-					Assert.AreEqual((double)(i + 1), (double)table[TestKeys[i]]);
+				for (int i = 0; i < _Keys.Count; ++i)
+					Assert.AreEqual((double)(i + 1), (double)table[_Keys[i]]);
 
 				// double key
-				Assert.AreEqual(TestDoubleValue, (string)table[TestDouble]);
+				Assert.AreEqual(_DoubleValue, (string)table[_Double]);
 
 				// - try reading missing values, which the metatable will intercept -
 
-				for (double i = TestArrayEnd+1; i <= TestArrayEnd*2; ++i)
-					Assert.AreEqual(TestMetaRead, table[i]);
+				for (double i = _ArrayEnd+1; i <= _ArrayEnd*2; ++i)
+					Assert.AreEqual(_MetaRead, table[i]);
 
-				foreach (string k in TestEmptyKeys)
-					Assert.AreEqual(TestMetaRead, table[k]);
+				foreach (string k in _EmptyKeys)
+					Assert.AreEqual(_MetaRead, table[k]);
 			}
 		}
 
@@ -88,7 +89,7 @@ namespace LuaInterfaceTest
 				// - try overwriting values, which the metatable can't intercept -
 
 				// array
-				for (double i = 1; i <= TestArrayEnd; ++i)
+				for (double i = 1; i <= _ArrayEnd; ++i)
 				{
 					Assert.AreEqual(i, (double)table[i]);
 					table[i] = 12;
@@ -96,33 +97,33 @@ namespace LuaInterfaceTest
 				}
 
 				// associative array
-				for (int i = 0; i < TestKeys.Count; ++i)
+				for (int i = 0; i < _Keys.Count; ++i)
 				{
-					var k = TestKeys[i];
+					var k = _Keys[i];
 					Assert.AreEqual((double)(i + 1), (double)table[k]);
 					table[k] = 12;
 					Assert.AreEqual(12.0, (double)table[k]);
 				}
 
 				// double key
-				Assert.AreEqual(TestDoubleValue, (string)table[TestDouble]);
-				table[TestDouble] = 12;
-				Assert.AreEqual(12.0, (double)table[TestDouble]);
+				Assert.AreEqual(_DoubleValue, (string)table[_Double]);
+				table[_Double] = 12;
+				Assert.AreEqual(12.0, (double)table[_Double]);
 
 				// - try adding values, which the metatable will intercept -
 
-				for (double i = TestArrayEnd + 1; i <= TestArrayEnd * 2; ++i)
+				for (double i = _ArrayEnd + 1; i <= _ArrayEnd * 2; ++i)
 				{
 					Assert.AreEqual(null, table[i]);
 					table[i] = 12;
-					Assert.AreEqual(TestMetaWrite, table[i]);
+					Assert.AreEqual(_MetaWrite, table[i]);
 				}
 
-				foreach (string k in TestEmptyKeys)
+				foreach (string k in _EmptyKeys)
 				{
 					Assert.AreEqual(null, table[k]);
 					table[k] = 12;
-					Assert.AreEqual(TestMetaWrite, table[k]);
+					Assert.AreEqual(_MetaWrite, table[k]);
 				}
 			}
 		}
@@ -135,27 +136,27 @@ namespace LuaInterfaceTest
 				// - try reading normal values, which the metatable can't intercept either way -
 
 				// array
-				for (int i = 1; i <= TestArrayEnd; ++i)
+				for (int i = 1; i <= _ArrayEnd; ++i)
 					Assert.AreEqual((double)i, (double)table.RawGet(i));
-				for (double i = 1; i <= TestArrayEnd; ++i)
+				for (double i = 1; i <= _ArrayEnd; ++i)
 					Assert.AreEqual(i, (double)table.RawGet(i));
 
 				// associative array
-				for (int i = 0; i < TestKeys.Count; ++i)
-					Assert.AreEqual((double)(i + 1), (double)table.RawGet(TestKeys[i]));
+				for (int i = 0; i < _Keys.Count; ++i)
+					Assert.AreEqual((double)(i + 1), (double)table.RawGet(_Keys[i]));
 
 				// double key
-				Assert.AreEqual(TestDoubleValue, (string)table.RawGet(TestDouble));
+				Assert.AreEqual(_DoubleValue, (string)table.RawGet(_Double));
 
 				// - try reading missing values, which the metatable would intercept if we weren't using RawGet -
 
-				for (int i = TestArrayEnd+1; i <= TestArrayEnd*2; ++i)
+				for (int i = _ArrayEnd+1; i <= _ArrayEnd*2; ++i)
 					Assert.IsNull(table.RawGet(i));
 
-				foreach (string k in TestEmptyKeys)
+				foreach (string k in _EmptyKeys)
 					Assert.IsNull(table.RawGet(k));
 
-				for (double i = TestArrayEnd+1; i <= TestArrayEnd*2; ++i)
+				for (double i = _ArrayEnd+1; i <= _ArrayEnd*2; ++i)
 					Assert.IsNull(table.RawGet(i));
 			}
 		}
@@ -166,9 +167,9 @@ namespace LuaInterfaceTest
 			using (var table = (LuaTable)lua.DoString("return newTestTable(false, true)", "@Table.cs.RawSet.lua")[0])
 			{
 				// array
-				for (int i = 1; i <= TestArrayEnd*2; ++i)
+				for (int i = 1; i <= _ArrayEnd*2; ++i)
 				{
-					if (i <= TestArrayEnd)
+					if (i <= _ArrayEnd)
 						Assert.AreEqual((double)i, (double)table[i]);
 					else
 						Assert.IsNull(table[i]);
@@ -179,11 +180,11 @@ namespace LuaInterfaceTest
 				}
 
 				// associative array
-				var keys = Enumerable.Concat(TestKeys, TestEmptyKeys).ToArray();
+				var keys = Enumerable.Concat(_Keys, _EmptyKeys).ToArray();
 				for (int i = 0; i < keys.Length; ++i)
 				{
 					string k = keys[i];
-					if (i < TestKeys.Count)
+					if (i < _Keys.Count)
 						Assert.AreEqual((double)(i + 1), (double)table[k]);
 					else
 						Assert.IsNull(table[k]);
@@ -192,13 +193,13 @@ namespace LuaInterfaceTest
 				}
 
 				// double key
-				Assert.AreEqual(TestDoubleValue, (string)table[TestDouble]);
-				table.RawSet(TestDouble, 12);
-				Assert.AreEqual(12.0, (double)table[TestDouble]);
+				Assert.AreEqual(_DoubleValue, (string)table[_Double]);
+				table.RawSet(_Double, 12);
+				Assert.AreEqual(12.0, (double)table[_Double]);
 
-				Assert.IsNull(table[TestEmptyDouble]);
-				table.RawSet(TestEmptyDouble, 12);
-				Assert.AreEqual(12.0, table[TestEmptyDouble]);
+				Assert.IsNull(table[_EmptyDouble]);
+				table.RawSet(_EmptyDouble, 12);
+				Assert.AreEqual(12.0, table[_EmptyDouble]);
 			}
 		}
 	}
