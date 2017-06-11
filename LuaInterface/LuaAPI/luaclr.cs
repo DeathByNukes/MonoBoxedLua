6897fb0029cc9bde1ae7b16b878cd0ea0aea506a
DeathByNukes <DeathByNukes@gmail.com>
2017-06-11 16:59:48 -0500

    Add LuaAPI.luaclr class for storing references to CLR objects directly in Lua.

---
+++ LuaInterface/LuaAPI/luaclr.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace LuaInterface.LuaAPI
+{
+	using size_t = System.UIntPtr;
+
+	public static class luaclr
+	{
+		/// <summary>.NET 4.5 AggressiveInlining. Should be auto discarded on older build targets or otherwise ignored.</summary>
+		const MethodImplOptions INLINE = (MethodImplOptions) 0x0100;
+
+		#region reference system
+
+		/// <summary>[-0, +1, m] Pushes a new userdata holding a reference to the specified object. The object can be retrieved by calling <see cref="getref"/>. The object cannot be garbage collected until <see cref="freeref"/> is called.<para>The userdata pushed by this function does not come with a metatable; you should assign it a metatable created by <see cref="newrefmeta(lua.State,int)"/> or <see cref="newrefmeta(lua.State,string,int)"/>.</para><para>The *ref functions are thread-safe; they can used by Lua instances running on different threads.</para></summary>
+		public static unsafe void newref(lua.State L, object o, GCHandleType type = GCHandleType.Normal)
+		{
+			IntPtr* contents = (IntPtr*) lua.newuserdata(L, new UIntPtr((void*) sizeof(IntPtr))).ToPointer();
+			*contents = GCHandle.ToIntPtr(GCHandle.Alloc(o, type));
+		}
+		/// <summary>[-0, +0, -] Frees a reference created by <see cref="newref"/> so the object can be garbage collected. The value at <paramref name="index"/> MUST be a reference created by <see cref="newref"/>; memory corruption or undefined behavior may result for any other value.<para>The *ref functions are thread-safe; they can used by Lua instances running on different threads.</para></summary>
+		public static unsafe void freeref(lua.State L, int index)
+		{
+			Debug.Assert(luaclr.isreflike(L, index));
+			IntPtr* contents = (IntPtr*) lua.touserdata(L, index);
+			var value = *contents;
+			if (value == default(IntPtr))
+				return;
+			*contents = default(IntPtr);
+			GCHandle.FromIntPtr(value).Free();
+		}
+		/// <summary>[-0, +0, -] Retrieves the object from a reference created by <see cref="newref"/>. The value at <paramref name="index"/> MUST be a reference created by <see cref="newref"/>; memory corruption or undefined behavior may result for any other value.<para>Returns null if the reference has been freed.</para><para>The *ref functions are thread-safe; they can used by Lua instances running on different threads.</para></summary>
+		public static unsafe object getref(lua.State L, int index)
+		{
+			Debug.Assert(luaclr.isreflike(L, index));
+			IntPtr* contents = (IntPtr*) lua.touserdata(L, index);
+			var handle = *contents;
+			return handle == default(IntPtr) ? null : GCHandle.FromIntPtr(handle).Target;
+		}
+		/// <summary>[-0, +0, -] Checks if an object is a userdata and is the same size as a reference. Does not check the metatable. See also: <see cref="isref"/></summary>
+		public static unsafe bool isreflike(lua.State L, int index)
+		{
+			return lua.type(L, index) == LUA.T.USERDATA
+				&& lua.objlen(L, index) == new UIntPtr((void*)sizeof(IntPtr))  ;
+		}
+
+		/// <summary>[-0, +1, m] Pushes a new table which contains the minimal fields necessary for a properly functioning ref metatable. (__gc and __metatable) <paramref name="field_count"/> is the number of fields you plan to add, so more space can be pre-allocated.</summary>
+		public static void newrefmeta(lua.State L, int field_count)
+		{
+			luaL.checkstack(L, 3, "luaclr.newrefmeta");
+			lua.createtable(L, 0, field_count + 3);
+
+			lua.pushcfunction(L, __gc);
+			lua.setfield(L, -2, "__gc");
+
+			lua.pushboolean(L, false);
+			lua.setfield(L, -2, "__metatable");
+
+			lua.pushlightuserdata(L, luanet.gettag());
+			lua.pushboolean(L, true);
+			lua.rawset(L, -3);
+		}
+		static readonly lua.CFunction __gc = L =>
+		{
+			if (luaclr.isref(L, 1))
+				luaclr.freeref(L, 1);
+			return 0;
+		};
+		/// <summary>[-0, +0, m] Checks if the value at <paramref name="index"/> is a reference-sized userdata with a metatable made by <see cref="newrefmeta"/>. See also: <see cref="isreflike"/></summary>
+		public static bool isref(lua.State L, int index)
+		{
+			luaL.checkstack(L, 2, "luaclr.isref");
+			index = luanet.absoluteindex(L, index);
+			if (!luaclr.isreflike(L, index) || !lua.getmetatable(L, index))
+				return false;
+			lua.pushlightuserdata(L, luanet.gettag());
+			lua.rawget(L, -2);
+			bool isref = lua.type(L, -1) != LUA.T.NIL;
+			lua.pop(L, 2);
+			return isref;
+		}
+		/// <summary>
+		/// <para>[-0, +1, m] <see cref="luaL.newmetatable"/> variant that uses <see cref="newrefmeta(lua.State,int)"/> to create the metatable.</para>
+		/// <para>luaL.newmetatable: If the registry already has the key tname, returns false. Otherwise, creates a new table to be used as a metatable for userdata, adds it to the registry with key tname, and returns true. In both cases pushes onto the stack the final value associated with tname in the registry.</para>
+		/// <para>luaclr.newrefmeta: Pushes a new table which contains the minimal fields necessary for a properly functioning ref metatable. (__gc and __metatable) <paramref name="field_count"/> is the number of fields you plan to add, so more space can be pre-allocated.</para>
+		/// </summary>
+		public static bool newrefmeta(lua.State L, string tname, int field_count)
+		{
+			lua.getfield(L, LUA.REGISTRYINDEX, tname);
+			if (lua.istable(L, -1))
+				return false;
+			lua.pop(L, 1);
+			luaclr.newrefmeta(L, field_count);
+			lua.pushvalue(L, -1);
+			lua.setfield(L, LUA.REGISTRYINDEX, tname);
+			return true;
+		}
+
+		#endregion
+
+
+		/// <summary>[-0, +1, m] Pushes a C function onto the stack just like lua_pushcfunction except the delegate will be automatically kept alive by storing an opaque userdata in an upvalue.</summary>
+		[MethodImpl(INLINE)] public static void pushcfunction(lua.State L, lua.CFunction f) { luaclr.pushcclosure(L, f, 0); }
+		/// <summary>[-n, +1, m] Pushes a new C closure onto the stack. <paramref name="n"/> can be up to 254; the last slot is reserved for a garbage collection userdata that keeps the delegate alive.</summary>
+		public static void pushcclosure(lua.State L, lua.CFunction f, int n)
+		{
+			Debug.Assert(f != null);
+			Debug.Assert(n <= 254 && n >= 0);
+			luaL.checkstack(L, 2, "luaclr.pushcclosure");
+
+			luaclr.newref(L, f);
+			luaclr.newrefmeta(L, "LuaCLR CFunction anchor", 0);
+			// nothing to add to this metatable other than gc, it just needs to exist and then not exist
+			lua.setmetatable(L, -2);
+
+			lua.pushcclosure(L, f, n+1);
+		}
+	}
+}
--- LuaInterfaceTest/Profiling.cs
+++ LuaInterfaceTest/Profiling.cs
@@ -1,7 +1,9 @@
 ﻿#if false
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using LuaInterface;
 using LuaInterface.LuaAPI;
 
@@ -91,6 +93,167 @@ namespace LuaInterfaceTest
 					Lua -> stdcall -> CLR: 731ms
 					Lua -> cdecl -> CLR: 420ms
 					Lua -> Lua: 468ms
+				*/
+			}
+		}
+
+		[TestMethod] public void GCHandles()
+		{
+			const uint iterations = 2048*2048;
+			Trace.WriteLine(iterations.ToString("N0") + " iterations");
+
+			const int pool_size = 100;
+
+			var pool = new Dictionary<int, object>();
+			var indices = new int[pool_size];
+			for (int i = 0; i < indices.Length; ++i)
+				pool[indices[i] = i] = new object();
+
+			var handles = new IntPtr[pool_size];
+			for (int i = 0; i < handles.Length; ++i)
+				handles[i] = GCHandle.ToIntPtr(GCHandle.Alloc(new object()));
+
+
+			var timer = Stopwatch.StartNew();
+			for (uint n = 0; n < iterations; ++n)
+			for (int i = 0; i < indices.Length; ++i)
+				GC.KeepAlive(pool[indices[i]]);
+			timer.Stop();
+			Display(timer, "Dictionary");
+
+
+			timer.Reset();timer.Start();
+			for (uint n = 0; n < iterations; ++n)
+			for (int i = 0; i < handles.Length; ++i)
+				GC.KeepAlive(GCHandle.FromIntPtr(handles[i]).Target);
+			timer.Stop();
+			Display(timer, "GCHandle");
+
+				
+			for (int i = 0; i < handles.Length; ++i)
+				GCHandle.FromIntPtr(handles[i]).Free();
+
+			/*
+				4,194,304 iterations
+				Dictionary: 6266ms
+				GCHandle: 5090ms
+
+				release mode:
+				4,194,304 iterations
+				Dictionary: 5580ms
+				GCHandle: 4731ms
+
+				(each iteration is a pool of 100)
+			*/
+		}
+
+		[TestMethod] public void NewUserdata()
+		{
+			using (var luainterface = new Lua())
+			{
+				const int iterations = 2048*2048;
+				Trace.WriteLine(iterations.ToString("N0") + " iterations");
+
+				var o = new object();
+				var L = luanet.getstate(luainterface);
+				lua.createtable(L, iterations, 0);
+
+				var timer = Stopwatch.StartNew();
+				for (int n = 0; n < iterations; ++n)
+				{
+					luaclr.newref(L, o);
+					luaclr.newrefmeta(L, "new userdata test object", 0);
+					lua.setmetatable(L, -2);
+					lua.rawseti(L, -2, n+1);
+				}
+				timer.Stop();
+				Display(timer, "new refs added to table");
+
+				/*
+					4,194,304 iterations
+					new refs added to table: 5169ms
+
+					release mode:
+					4,194,304 iterations
+					new refs added to table: 2049ms
+				*/
+			}
+		}
+
+		[TestMethod] public void Indexer()
+		{
+			const uint iterations = 128*1024;
+			using (var li = new Lua())
+			{
+				var L = luanet.getstate(li);
+				Trace.WriteLine(iterations.ToString("N0") + " iterations");
+
+				const int pool_size = 100;
+
+				lua.createtable(L, pool_size, 0);
+				lua.createtable(L, 0, pool_size);
+				lua.createtable(L, 0, pool_size);
+				var i_int = new int[pool_size];
+				var i_str = new string[pool_size];
+				var i_lud = new IntPtr[pool_size];
+				for (int i = 0; i < pool_size; ++i)
+				{
+					lua.pushboolean(L, true);
+					lua.rawseti(L, 1, i_int[i] = i);
+
+					lua.pushboolean(L, true);
+					lua.setfield(L, 2, i_str[i] = i.ToString());
+
+					lua.pushboolean(L, true);
+					lua.pushlightuserdata(L, i_lud[i] = new IntPtr(i));
+					lua.settable(L, 3);
+				}
+
+
+				var timer = Stopwatch.StartNew();
+				for (uint n = 0; n < iterations; ++n)
+				for (int i = 0; i < i_int.Length; ++i)
+				{
+					lua.rawgeti(L, 1, i_int[i]);
+					lua.pop(L, 1);
+				}
+				timer.Stop();
+				Display(timer, "int");
+
+				
+				timer.Reset();timer.Start();
+				for (uint n = 0; n < iterations; ++n)
+				for (int i = 0; i < i_str.Length; ++i)
+				{
+					lua.getfield(L, 2, i_str[i]);
+					lua.pop(L, 1);
+				}
+				timer.Stop();
+				Display(timer, "string");
+
+				
+				timer.Reset();timer.Start();
+				for (uint n = 0; n < iterations; ++n)
+				for (int i = 0; i < i_lud.Length; ++i)
+				{
+					lua.pushlightuserdata(L, i_lud[i]);
+					lua.rawget(L, 3);
+					lua.pop(L, 1);
+				}
+				timer.Stop();
+				Display(timer, "lightuserdata");
+
+				/*
+					131,072 iterations
+					int: 1824ms
+					string: 4082ms
+					lightuserdata: 3005ms
+
+					release mode:
+					131,072 iterations
+					int: 471ms
+					string: 1316ms
+					lightuserdata: 729ms
 				*/
 			}
 		}
